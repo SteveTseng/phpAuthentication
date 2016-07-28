b1aeeac6847fc161d3eb6c2250ff6dd9f729bc71
Steve Tseng <stevetseng@live.com>
2016-07-28 15:56:31 -0700

    first commit

---
+++ app/start.php
@@ -0,0 +1,31 @@
+<?php
+
+use Slim\Slim;
+use Noodlehaus\Config;
+
+use phpAuth\User\User;
+
+session_cache_limiter(false);
+session_start();
+
+ini_set('display_errors', 'On');
+
+define('INC_ROOT', dirname(__DIR__));
+
+require INC_ROOT . '/vendor/autoload.php';
+
+$app = new Slim([
+	'mode' => file_get_contents(INC_ROOT . '/mode.php')
+]);
+
+$app->configureMode($app->config('mode'), function() use($app){
+	$app->config = Config::load(INC_ROOT . "/app/config/{$app->mode}.php");
+});
+
+require 'database.php';
+
+$app->container->set('user', function(){
+	return new User;
+});
+
+var_dump($app->user);
